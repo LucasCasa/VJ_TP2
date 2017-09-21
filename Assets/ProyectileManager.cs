73f8b287b0480b5f07682549408aac8c6ad60c01
LucasCasa <lcasagrande@itba.edu.ar>
2017-09-21 11:49:34 -0300

    trayectorias para todos

--- Assets/ProyectileManager.cs
+++ Assets/ProyectileManager.cs
@@ -10,6 +10,7 @@ public class ProyectileManager : MonoBehaviour {
 	public float interval;
 	public float distance;
 	public float force;
+	public float time;
 
 	private List<GameObject> list = new List<GameObject>();
 
@@ -34,7 +35,26 @@ public class ProyectileManager : MonoBehaviour {
 			return;
 		go.SetActive (true);
 		Vector2 pos = Random.insideUnitCircle*distance;
-		go.transform.position = new Vector3 (pos.x, 4.0f, pos.y+20);
-		go.GetComponent<Rigidbody> ().AddForce (new Vector3 (-pos.x,0.0f,-pos.y)*force, ForceMode.Impulse);
+		go.transform.position = new Vector3 (pos.x, 0, pos.y);
+		Vector3 force = calcBallisticVelocityVector (go.transform.position, new Vector3 (0, 0, 0), 45);
+		var g = Physics.gravity.magnitude; // get the gravity value
+		var hSpeed = Vector3.Distance(go.transform.position,new Vector3(0,0,0)) / time; // calculate the horizontal speed
+		var vspeed = 0.5f*g*time;
+		go.GetComponent<Rigidbody> ().velocity = hSpeed*Vector3.Normalize(-go.transform.position) + new Vector3(0,vspeed,0);
+		//go.GetComponent<Rigidbody> ().AddForce (new Vector3 (-pos.x,0.0f,-pos.y)*force, ForceMode.Impulse);
+	}
+
+	private Vector3 calcBallisticVelocityVector(Vector3 source, Vector3 target, float angle){
+		Vector3 direction = target - source;                            
+		float h = direction.y;                                           
+		direction.y = 0;                                               
+		float distance = direction.magnitude;                           
+		float a = angle * Mathf.Deg2Rad;                                
+		direction.y = distance * Mathf.Tan(a);                            
+		distance += h/Mathf.Tan(a);                                      
+
+		// calculate velocity
+		float velocity = Mathf.Sqrt(distance * Physics.gravity.magnitude / Mathf.Sin(2*a));
+		return velocity * direction.normalized;    
 	}
 }
