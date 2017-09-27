91fc0dc0df07335efca5a7759e6ae75723711baf
Nicolas Kuyumciyan <nkuyumciyan@medallia.com>
2017-09-27 20:49:35 -0300

    Powerups and a whole other game

---
+++ Assets/PowerUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour {
+
+	public float height;
+	public float axisDistance;
+	public float duration;
+	public float minScale;
+
+	// Use this for initialization
+	void Start () {
+		gameObject.transform.position = new Vector3 (Random.Range(-axisDistance,axisDistance), height, Random.Range(-axisDistance,axisDistance));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float reduction = Time.deltaTime/duration;
+		gameObject.transform.localScale -= new Vector3 (reduction, reduction, reduction);
+		if (gameObject.transform.localScale.x <= minScale)
+			Destroy (gameObject);
+	}
+}
---
+++ Assets/PowerUpCreator.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpCreator : MonoBehaviour
+{
+	public float maxTime;
+	public float minTime;
+
+	//current time
+	private float time;
+
+	//The time to spawn the object
+	private float spawnTime;
+
+	public GameObject[] powerUps;
+
+	void Start()
+	{
+		SetRandomTime();
+		time = minTime;
+	}
+
+	void FixedUpdate()
+	{
+		//Counts up
+		time += Time.deltaTime;
+
+		//Check if its the right time to spawn the object
+		if (time >= spawnTime)
+		{
+			SpawnPowerUp();
+			time = 0;
+			SetRandomTime();
+		}
+
+	}
+
+
+	//Spawns the object and resets the time
+	void SpawnPowerUp()
+	{
+		Instantiate(RandomPowerUp());
+	}
+
+	GameObject RandomPowerUp()
+	{
+		int r = Mathf.FloorToInt(Random.Range(0, powerUps.Length));
+		return powerUps [r];
+	}
+
+	//Sets the random time between minTime and maxTime
+	void SetRandomTime()
+	{
+		spawnTime = Random.Range(minTime, maxTime);
+	}
+
+}
+
