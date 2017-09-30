edf4336095b8df590721fda1cb8b0a5cf1079609
Lucas Casagrande <lcasagrande@itba.edu.ar>
2017-09-30 14:48:44 -0300

    Ui (#1)
    * Balas temporales, vida se muestra
    
    * me olvide de guardar la escena
    
    * Icons
    
    * UI casi terminada

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
@@ -20,11 +21,23 @@ public class CameraMovement : MonoBehaviour {
     public WFX_LightFlicker bulletLightEffect;
     public GameObject explosion;
 
+	public RectTransform bulletBar;
+	public int bulletCapacity;
+	public Text health;
+
+	private Vector2 bulletWidth = new Vector2 (25, 0);
+
+	public int life = 10;
+
     //private Vector2 lastMoouse = new Vector2();
     // Use this for initialization
     void Start () {
         //lastMoouse = Input.mousePosition;
         Cursor.lockState = CursorLockMode.Locked;
+		bulletBar.sizeDelta = new Vector2 (bulletWidth.x * bulletCapacity, 77);
+		Camera cam = gameObject.GetComponent<Camera> ();
+		bulletBar.position = new Vector3 (cam.pixelWidth - bulletWidth.x * bulletCapacity - 30, 15, 0);
+		health.text = life.ToString();
 	}
 
     void Update() {
@@ -36,15 +49,19 @@ public class CameraMovement : MonoBehaviour {
         if(Input.GetKeyDown(KeyCode.P)) {
             Cursor.lockState = CursorLockMode.None;
         }
-        if (Input.GetMouseButtonDown(0)) {
+		if (bulletBar.sizeDelta.x / bulletWidth.x < bulletCapacity) {
+			bulletBar.sizeDelta = bulletBar.sizeDelta + bulletWidth * Time.deltaTime;
+		}
+		if (Input.GetMouseButtonDown(0) && bulletBar.sizeDelta.x >= bulletWidth.x) {
             Debug.Log("Firee");
             Recoil();
             muzzle.Play();
             sound.Play();
+			bulletBar.sizeDelta = bulletBar.sizeDelta - bulletWidth;
             bulletLightEffect.startEmiting();
             Ray ray = new Ray(transform.position, transform.forward);
             RaycastHit hit;
-            if (Physics.Raycast(ray,out hit, 200)) {
+            if (Physics.Raycast(ray,out hit, 500)) {
 				if (hit.collider.tag == "target") {
 					hit.collider.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
 					hit.collider.gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
@@ -72,4 +89,9 @@ public class CameraMovement : MonoBehaviour {
         //Debug.Log((Input.mousePosition.y - lastMoouse.y) + " " + (Input.mousePosition.x - lastMoouse.x));
         transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
     }
+	void OnTriggerEnter(Collider other) {
+		other.gameObject.SetActive(false);
+		life--;
+		health.text = life.ToString();
+	}
 }
