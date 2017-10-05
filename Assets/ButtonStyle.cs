ad31e643fbf09ee2dc3a60d8c1eebf1f176b3699
LucasCasa <lcasagrande@itba.edu.ar>
2017-10-05 11:13:57 -0300

    me faltron un par de archivos

---
+++ Assets/ButtonStyle.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;  
+using System.Collections;  
+using UnityEngine.EventSystems;  
+using UnityEngine.UI;
+
+public class ButtonStyle : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+	public Text theText;
+	Color highlighted = new Color(1,1,0); //Or however you do your color
+	Color other = new Color(0.8f,0.8f,0); //Or however you do your color
+	void Start(){
+		theText = this.GetComponentInChildren<Text> ();
+		theText.color = other;
+	}
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		theText.color = highlighted;
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		theText.color = other;
+	}
+
+	void Continue(){
+		
+	}
+
+	void GotoMenu(){
+		//TODO
+	}
+	void Quit (){
+
+	}
+}
