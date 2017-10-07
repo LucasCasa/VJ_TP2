69af63713add22dae6d36c97d4d56ba6373bcb91
Maggie Vega <mvega@itba.edu.ar>
2017-10-07 18:06:01 -0300

    Starting Menu and Settings

---
+++ Assets/MenuButtons.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public Text Menu;
+
+    Color highlighted = new Color(1, 1, 0); //Or however you do your color
+    Color other = new Color(0.8f, 0.8f, 0); //Or however you do your color
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Menu.color = highlighted;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Menu.color = other;
+    }
+
+    public void goToGame()
+    {
+        SceneManager.LoadScene("game");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void goToSettings()
+    {
+        SceneManager.LoadScene("settings");
+    }
+
+    // Use this for initialization
+	void Start () {
+		Menu = this.GetComponentInChildren<Text>();
+		Menu.color = other;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
