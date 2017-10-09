14987a2b8a53770033509aae2f51220e1fc68934
Maggie Vega <mvega@itba.edu.ar>
2017-10-09 13:52:38 -0300

    Camara se mueve en forma de cuadrado por el terrain

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
 
-    private Vector3 offset = new Vector3(10,0);
+    private Vector3 offset;
+    private float increment = 0.2f;
 
 	// Use this for initialization
 	void Start () {
-		
+        offset = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (offset.z.CompareTo(0) == 0 && offset.x.CompareTo(400) < 0)
+        {
+            offset.z = 0;
+            offset.x += increment;
+
+        }else if (offset.z.CompareTo(400) == 0 && offset.x.CompareTo(0) > 0)
+        {
+            offset.z = 0;
+            offset.x -= increment;   
+        }else if (offset.x.CompareTo(400) == 0 && offset.z.CompareTo(400) < 0)
+        {
+			offset.z += increment;
+            offset.x = 400;
+        }else if (offset.x.CompareTo(0) == 0 && offset.z.CompareTo(0) > 0)
+        {
+			offset.z -= increment;
+			offset.x = 0;
+        }
+ 
+		transform.position = offset;
 	}
 }
