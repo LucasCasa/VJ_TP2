f1924d448c4aaec1a3d5dd9e8d37e8f78b2df290
Maggie Vega <mvega@itba.edu.ar>
2017-10-11 09:27:44 -0300

    Se ve que dificultad esta seleccionada

--- Assets/MenuButtons.cs
+++ Assets/MenuButtons.cs
@@ -7,6 +7,39 @@ using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour
 {
+    public GameObject EasyB;
+	public GameObject MediumB;
+	public GameObject HardB;
+	public GameObject ImpossibleB;
+
+	public void diffSelected(int diff){
+        switch (diff){
+            case 1:
+                EasyB.SetActive(true);
+                MediumB.SetActive(false);
+                HardB.SetActive(false);
+                ImpossibleB.SetActive(false);
+                break;
+            case 2:
+				EasyB.SetActive(false);
+				MediumB.SetActive(true);
+				HardB.SetActive(false);
+				ImpossibleB.SetActive(false);
+                break;
+			case 3:
+				EasyB.SetActive(false);
+				MediumB.SetActive(false);
+				HardB.SetActive(true);
+				ImpossibleB.SetActive(false);
+                break;
+            case 4:
+				EasyB.SetActive(false);
+				MediumB.SetActive(false);
+				HardB.SetActive(false);
+				ImpossibleB.SetActive(true);
+                break;
+        }
+    }
 
     public void goToGame()
     {
