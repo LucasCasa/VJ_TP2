691d055b6656158b381dcc4212e7e57edd7030a2
Maggie Vega <mvega@itba.edu.ar>
2017-10-11 15:51:29 -0300

    Remembers settings

--- Assets/Scripts/SettingButton.cs
+++ Assets/Scripts/SettingButton.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class SettingButton : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject EasyB;
+    public GameObject MediumB;
+    public GameObject HardB;
+    public GameObject ImpossibleB;
+
+    public void diffSelected(int diff)
+    {
+        switch (diff)
+        {
+            case 1:
+                EasyB.SetActive(true);
+                MediumB.SetActive(false);
+                HardB.SetActive(false);
+                ImpossibleB.SetActive(false);
+                break;
+            case 2:
+                EasyB.SetActive(false);
+                MediumB.SetActive(true);
+                HardB.SetActive(false);
+                ImpossibleB.SetActive(false);
+                break;
+            case 4:
+                EasyB.SetActive(false);
+                MediumB.SetActive(false);
+                HardB.SetActive(true);
+                ImpossibleB.SetActive(false);
+                break;
+            case 8:
+                EasyB.SetActive(false);
+                MediumB.SetActive(false);
+                HardB.SetActive(false);
+                ImpossibleB.SetActive(true);
+                break;
+        }
+    }
+
+    void Awake()
+    {
+        float difficulty = GameObject.Find("GameState").GetComponent<GameState>().getDifficulty();
+        diffSelected((int)(difficulty * 4));
+    }
+
 	//Se que hacer un find es costoso, pero para un menu no deberia ser problema
 	public void setDificulty(float dif){
 		Debug.Log ("Seteo Dificultad " + dif );
